c1440757f3afb7906babcdec1165d1e7d27da35a
Aude Lejuez <dusterherz@gmail.com>
2016-08-17 13:19:49 +0200

    Add Slashscreen

---
+++ TimePie/app/src/main/java/com/dusterherz/timepie/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dusterherz.timepie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dusterherz on 17/08/16.
+ * Main Activity of the app with the menu
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
