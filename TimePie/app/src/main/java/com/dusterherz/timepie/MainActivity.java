a669aca7a4c25aadefbe30fab2e385e406beb093
Aude Lejuez <dusterherz@gmail.com>
2016-08-22 20:03:54 +0200

    Create selector for chronometer

--- TimePie/app/src/main/java/com/dusterherz/timepie/MainActivity.java
+++ TimePie/app/src/main/java/com/dusterherz/timepie/MainActivity.java
@@ -1,16 +1,36 @@
 package com.dusterherz.timepie;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
-/**
- * Created by dusterherz on 17/08/16.
- * Main Activity of the app with the menu
- */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NumberPicker hours = (NumberPicker) findViewById(R.id.numberPickerHours);
+        NumberPicker minutes = (NumberPicker) findViewById(R.id.numberPickerMinutes);
+        NumberPicker seconds = (NumberPicker) findViewById(R.id.numberPickerSeconds);
+
+        hours.setMaxValue(24);
+        hours.setMinValue(0);
+        minutes.setMaxValue(60);
+        hours.setMinValue(0);
+        seconds.setMaxValue(60);
+        seconds.setMinValue(0);
+    }
+
+    public void startChronometer(View view) {
+        TextView textView = new TextView(this);
+        textView.setText("Yay !");
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.relativeLayoutMainActivity);
+        layout.addView(textView);
     }
 }
